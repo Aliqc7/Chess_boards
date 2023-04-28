bd26b033848a2b30a0625ee947eb04a4c057f4db
Ali <ali.zolghadr@gmail.com>
2023-04-28 22:49:35 +1000

    Crop function. Capture bothe colors

--- chesspi_utils.py
+++ chesspi_utils.py
@@ -1,27 +1,41 @@
 import cv2
 import math
+import os
 
 
 
 def extract_squares_form_starting_position(image_name, image_number):
-    piece_list = ["Rook", "Knight", "Bishop", "Queen", "King"]
+    piece_list = ["Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"]
+    for i in range(5):
+        if not os.path.exists(f"images/{piece_list[i]}"):
+            os.makedirs(f"images/{piece_list[i]}")
+    if not os.path.exists("images/Pawn"):
+        os.makedirs("images/Pawn")
+    if not os.path.exists(f"images/Blank"):
+        os.makedirs("images/Blank")
     img = cv2.imread(image_name)
     img_shape = img.shape
     square_side = int(img_shape[0]/8)
-    cropped_start = math.floor(0.05*square_side)
-    cropped_finish = math.floor(0.95*square_side)
-    for i in range(len(piece_list)):
-        square_b = img[0: square_side, i*square_side:(i+1)*square_side]
-        square_w = img[7 * square_side: 8 * square_side, i*square_side:(i+1)*square_side]
-        square_b = square_b[cropped_start: cropped_finish, cropped_start: cropped_finish]
-        square_w = square_w[cropped_start: cropped_finish, cropped_start: cropped_finish]
-        cv2.imwrite(f"images/{piece_list[i]}/Black_{piece_list[i]}{image_number}.jpg", square_b)
-        cv2.imwrite(f"images/{piece_list[i]}/White_{piece_list[i]}{image_number}.jpg", square_w)
-    cv2.imwrite(f"images/Pawn/Black_Pawn{image_number}.jpg", img[square_side: 2 * square_side, 0:square_side])
-    cv2.imwrite(f"images/Pawn/White_Pawn{image_number}.jpg", img[6*square_side: 7 * square_side, 0:square_side])
-    cv2.imwrite(f"images/Blank/Blank_White{image_number}.jpg", img[2 * square_side: 3 * square_side, 0:square_side])
-    cv2.imwrite(f"images/Blank/Blank_Black{image_number}.jpg", img[3 * square_side: 4 * square_side, 0:square_side])
+    crop_ratio = 0.05
+    for i, piece in enumerate(piece_list):
+        square_b = crop_image(img[0: square_side, i*square_side:(i+1)*square_side], square_side, crop_ratio)
+        square_w = crop_image(img[7 * square_side: 8 * square_side, i*square_side:(i+1)*square_side], square_side, crop_ratio)
+        cv2.imwrite(f"images/{piece}/Black_{piece}{i}{image_number}.jpg", square_b)
+        cv2.imwrite(f"images/{piece}/White_{piece}{i}{image_number}.jpg", square_w)
+    cv2.imwrite(f"images/Pawn/Black_Pawn{image_number}.jpg", crop_image(img[square_side: 2 * square_side, 0:square_side], square_side, crop_ratio))
+    cv2.imwrite(f"images/Pawn/White_Pawn{image_number}.jpg", crop_image(img[6*square_side: 7 * square_side, 0:square_side], square_side, crop_ratio))
+    cv2.imwrite(f"images/Pawn/Black_Pawn2{image_number}.jpg", crop_image(img[square_side: 2 * square_side, square_side:2*square_side], square_side, crop_ratio))
+    cv2.imwrite(f"images/Pawn/White_Pawn2{image_number}.jpg", crop_image(img[6*square_side: 7 * square_side, square_side:2*square_side], square_side, crop_ratio))
+    cv2.imwrite(f"images/Blank/Blank_White{image_number}.jpg", crop_image(img[2 * square_side: 3 * square_side, 0:square_side], square_side, crop_ratio))
+    cv2.imwrite(f"images/Blank/Blank_Black{image_number}.jpg", crop_image(img[3 * square_side: 4 * square_side, 0:square_side], square_side, crop_ratio))
 
 
-#extract_squares_form_starting_position("chess_board.png")
+def crop_image(img, side, ratio_on_each_side):
+    cropped_start = math.floor(ratio_on_each_side*side)
+    cropped_finish = math.floor((1-ratio_on_each_side)*side)
+    img = img[cropped_start: cropped_finish, cropped_start: cropped_finish]
+    return img
+
+
+#extract_squares_form_starting_position("images/chess_board2.jpg", 1)
 
