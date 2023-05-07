d3c89e6d863a6216ee2a3d4cd6a115a906ed804a
Ali <ali.zolghadr@gmail.com>
2023-05-07 13:05:03 +1000

    Restructured using functions

--- getboards.py
+++ getboards.py
@@ -8,79 +8,148 @@ from selenium.webdriver.support.ui import Select
 from PIL import Image
 from io import BytesIO
 
-options = Options()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get("https://chess.com/")
-driver.maximize_window()
-driver.implicitly_wait(10)
-login_button = driver.find_element(By.CLASS_NAME, "button.auth.login.ui_v5-button-component.ui_v5-button-primary")
-login_button.click()
-user_name = driver.find_element(By.ID, "username")
-user_name.send_keys(os.environ["CHESS_USERNAME"])
-password = driver.find_element(By.ID, "password")
-password.send_keys(os.environ["CHESS_PASSWORD"])
-board_and_pieces_tag = "a[href = 'https://www.chess.com/settings/board']"
-
-log_in = driver.find_element(By.ID, "login")
-log_in.click()
-settings = driver.find_element(By.CSS_SELECTOR, "span[class = 'icon-font-chess circle-gearwheel'")
-settings.click()
-
-
-board_and_pieces = driver.find_element(By.CSS_SELECTOR, board_and_pieces_tag)
-board_and_pieces.click()
-
-coordinates = Select(driver.find_element(By.ID, "board_pieces_showCoordinates"))
-coordinates.select_by_visible_text("Off")
-
-piece_style = Select(driver.find_element(By.ID, "board_pieces_gamePieceStyle"))
-piece_style_list = [option.text for option in piece_style.options]
-board_color = Select(driver.find_element(By.ID, "board_pieces_gameBoardColor"))
-board_color_list = [option.text for option in board_color.options]
+BASE_URL = "https://chess.com/"
+LOGIN_BUTTON = "button.auth.login.ui_v5-button-component.ui_v5-button-primary"
+USERNAME_ELEMENT_ID = "username"
+PASSWORD_ELEMENT_ID = "password"
+USERNAME_ENV_VARIABLE = "CHESS_USERNAME"
+PASSWORD_ENV_VARIABLE = "CHESS_PASSWORD"
+BOARD_AND_PIECES_TAG = "a[href = 'https://www.chess.com/settings/board']"
+LOGIN_ELEMENT_ID_IN_LOGIN_PAGE = "login"
+SETTINGS_CSS = "span[class = 'icon-font-chess circle-gearwheel'"
+COORDINATES_ELEMENT_ID = "board_pieces_showCoordinates"
+PIECE_STYLE_ELEMENT_ID = "board_pieces_gamePieceStyle"
+BOARD_COLOR_ELEMENT_ID = "board_pieces_gameBoardColor"
+SAVED_IMAGES_PATH = "boardimages"
+LAST_SAVED_PIECE_SET_FILE = "last_complete_piece_set.txt"
+SAVE_BUTTON_ELEMENT_ID = "board_pieces_save"
+PLAY_BUTTON_CSS = "a[href = 'https://www.chess.com/play']"
+BOARD_ELEMENT_ID = "board"
 
 
-if not os.path.exists("boardimages"):
-    os.mkdir("boardimages")
+def main():
+    driver = create_driver()
+    driver.implicitly_wait(10)
+    driver.get(BASE_URL)
+    driver.maximize_window()
+    login(driver)
+    go_to_settings_page(driver)
+    go_to_board_and_pieces_settings(driver)
+    turn_off_board_coordinate(driver)
+    piece_style = select_drop_down_element(driver, PIECE_STYLE_ELEMENT_ID)
+    board_color = select_drop_down_element(driver, BOARD_COLOR_ELEMENT_ID)
+    piece_style_list = create_options_list(piece_style)
+    board_color_list = create_options_list(board_color)
 
-if os.path.exists("last_complete_piece_set.txt"):
-    with open("last_complete_piece_set.txt", "r") as f:
-        last_saved_piece_set = f.read()
-    piece_loop_start_index = piece_style_list.index(last_saved_piece_set) + 1
-else:
-    piece_loop_start_index = 0
+    # To avoid repeating the process for already saved boards where an error interrupts the process
+    piece_loop_start_index = set_piece_loop_start_index(LAST_SAVED_PIECE_SET_FILE, piece_style_list)
+    # To limit the process to 2-D boards
+    piece_loop_end_index = set_piece_loop_end_index(piece_style_list)
+
+    os.makedirs(SAVED_IMAGES_PATH, exist_ok=True)
+    save_all_board_images(driver, piece_style_list, board_color_list, piece_loop_start_index, piece_loop_end_index)
 
 
-first_3d_index = [idx for idx, style in enumerate(piece_style_list) if '3D' in style][0]
+def login(driver):
 
-for piece_style_option in piece_style_list[piece_loop_start_index:first_3d_index]:
-    for board_color_option in board_color_list:
+    login_button = driver.find_element(By.CLASS_NAME, LOGIN_BUTTON)
+    login_button.click()
+    user_name = driver.find_element(By.ID, USERNAME_ELEMENT_ID)
+    user_name.send_keys(os.environ[USERNAME_ENV_VARIABLE])
+    password = driver.find_element(By.ID, PASSWORD_ELEMENT_ID)
+    password.send_keys(os.environ[PASSWORD_ENV_VARIABLE])
+    log_in = driver.find_element(By.ID, LOGIN_ELEMENT_ID_IN_LOGIN_PAGE)
+    log_in.click()
 
-        piece_style = Select(driver.find_element(By.ID, "board_pieces_gamePieceStyle"))
-        piece_style_list = [option.text for option in piece_style.options]
-        board_color = Select(driver.find_element(By.ID, "board_pieces_gameBoardColor"))
-        board_color_list = [option.text for option in board_color.options]
-        piece_style.select_by_visible_text(piece_style_option)
-        board_color.select_by_visible_text(board_color_option)
-        save_button = driver.find_element(By.ID, "board_pieces_save")
-        save_button.click()
-        play_button = driver.find_element(By.CSS_SELECTOR, "a[href = 'https://www.chess.com/play']")
-        play_button.click()
-        board = driver.find_element(By.ID, "board")
-        location = board.location
-        size = board.size
-        png = driver.get_screenshot_as_png()
-        img = Image.open(BytesIO(png))
-        left = location["x"]
-        top = location["y"]
-        right = location["x"] + size["width"]
-        bottom = location["y"] + size["height"]
-        img = img.crop((left, top, right, bottom))
-        img.save(f"boardimages/1{piece_style_option.replace(' ', '')}{board_color_option.replace(' ', '')}.png")
-        settings = driver.find_element(By.CSS_SELECTOR, "span[class = 'icon-font-chess circle-gearwheel'")
-        settings.click()
-        board_and_pieces = driver.find_element(By.CSS_SELECTOR, "a[href = 'https://www.chess.com/settings/board']")
-        board_and_pieces.click()
 
-    with open("last_complete_piece_set.txt", "w") as f:
-        f.write(piece_style_option)
+def go_to_settings_page(driver):
+    settings = driver.find_element(By.CSS_SELECTOR, SETTINGS_CSS)
+    settings.click()
+
+
+def go_to_board_and_pieces_settings(driver):
+    board_and_pieces = driver.find_element(By.CSS_SELECTOR, BOARD_AND_PIECES_TAG)
+    board_and_pieces.click()
+
+
+def turn_off_board_coordinate(driver):
+    coordinates = Select(driver.find_element(By.ID, COORDINATES_ELEMENT_ID))
+    coordinates.select_by_visible_text("Off")
+
+
+def select_drop_down_element(driver, element_id):
+    return Select(driver.find_element(By.ID, element_id))
+
+
+def click_save_settings(driver):
+    save_button = driver.find_element(By.ID, SAVE_BUTTON_ELEMENT_ID)
+    save_button.click()
+
+
+def click_play_button(driver):
+    play_button = driver.find_element(By.CSS_SELECTOR, PLAY_BUTTON_CSS)
+    play_button.click()
+
+
+def crop_image(screenshot_png, element):
+    location = element.location
+    size = element.size
+    img = Image.open(BytesIO(screenshot_png))
+    left = location["x"]
+    top = location["y"]
+    right = location["x"] + size["width"]
+    bottom = location["y"] + size["height"]
+    return img.crop((left, top, right, bottom))
+
+
+def save_image(img, piece_set_name, board_name):
+    img.save(
+        f"{SAVED_IMAGES_PATH}/{piece_set_name.replace(' ', '')}{board_name.replace(' ', '')}.png")
+
+
+def save_all_board_images(driver, piece_style_list, board_color_list, piece_loop_start_index, piece_loop_end_index):
+    for piece_style_name in piece_style_list[piece_loop_start_index:piece_loop_end_index]:
+        for board_color_name in board_color_list:
+            piece_style = select_drop_down_element(driver, PIECE_STYLE_ELEMENT_ID)
+            board_color = select_drop_down_element(driver, BOARD_COLOR_ELEMENT_ID)
+            piece_style.select_by_visible_text(piece_style_name)
+            board_color.select_by_visible_text(board_color_name)
+            click_save_settings(driver)
+            click_play_button(driver)
+            screenshot_png = driver.get_screenshot_as_png()
+            board = driver.find_element(By.ID, BOARD_ELEMENT_ID)
+            cropped_image = crop_image(screenshot_png, board)
+            save_image(cropped_image, piece_style_name, board_color_name)
+            go_to_settings_page(driver)
+            go_to_board_and_pieces_settings(driver)
+
+        with open(LAST_SAVED_PIECE_SET_FILE, "w") as f:
+            f.write(piece_style_name)
+
+
+def create_driver():
+    options = Options()
+    options.add_experimental_option("detach", True)
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+
+def create_options_list(dropdown_element):
+    return [option.text for option in dropdown_element.options]
+
+
+def set_piece_loop_start_index(saved_piece_file, piece_style_list):
+    if os.path.exists(saved_piece_file):
+        with open(saved_piece_file, "r") as f:
+            last_saved_piece_set = f.read()
+        start_index = piece_style_list.index(last_saved_piece_set) + 1
+    else:
+        start_index = 0
+    return start_index
+
+
+def set_piece_loop_end_index(piece_style_list):
+    return [idx for idx, style in enumerate(piece_style_list) if '3D' in style][0]
+
+
+if __name__ == '__main__':
+    main()
